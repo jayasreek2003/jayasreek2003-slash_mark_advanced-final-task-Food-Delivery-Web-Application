8ce3dcfbd87569c54b5797169f632429b5e26994
jayasreek2003 <jayasreek2003@gmail.com>
2024-03-23 12:46:51 +0530

    first commit

---
+++ shop/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    desc = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    image = models.ImageField(upload_to="shop/images", default="")
+
+    def __str__(self):
+        return self.product_name
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    userId = models.IntegerField(default=0)
+    amount = models.IntegerField(default=0)
+    name = models.CharField(max_length=90)
+    phone = models.CharField(max_length=111, default="")
+    timestamp = models.DateTimeField(default=timezone.now)
+
+
+class OrderUpdate(models.Model):
+    update_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.update_desc
